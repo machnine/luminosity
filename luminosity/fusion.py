b9eb193ff80436bd90ed099f9229ead716445b67
Mian Chen <mianchen@gmail.com>
2018-04-14 01:32:24 +0100

    minor format change

--- luminosity/fusion.py
+++ luminosity/fusion.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import json
 import pypyodbc
 import pandas as pd
@@ -15,8 +9,6 @@ import pandas as pd
 # ### 3. db -> new/overwrite/append etc.
 # ### 4. testing
 
-# In[2]:
-
 
 def read_fusion_connection_json(settings_json='fusion_settings.json'):
     '''
@@ -26,17 +18,12 @@ def read_fusion_connection_json(settings_json='fusion_settings.json'):
     return config
 
 
-# In[3]:
-
 
 def fusion_connection_string(config):
     '''create a connection string from config (dict)'''
     return config['connection_string'].format(server=config['server'],
                                              db_name=config['db_name'],
                                              password=config['password'])
-
-
-# In[4]:
 
 
 def update_fusion_connection_json(settings_json='fusion_settings.json', **kwargs):
@@ -64,18 +51,12 @@ def update_fusion_connection_json(settings_json='fusion_settings.json', **kwargs
         print(e)
 
 
-# In[5]:
-
-
 def connect_to_fusion(connection_string):
     try:
         return pypyodbc.connect(connection_string)
     except Exception as e:
         print(e)
         return None
-
-
-# In[18]:
 
 
 def read_data_from_fusion(patient_local_id, **kwargs):
@@ -105,55 +86,55 @@ def read_data_from_fusion(patient_local_id, **kwargs):
         session_date = f'and t.AddDT >= \'{session_date[0]}\' and t.AddDT < \'{session_date[1]}\''
     
     sql = f'''
-        select  p.patientid as patient_id
+        select  
+
+                p.patientid as patient_id
                 ,t.trayidname as session_name
                 ,s.sampleidname as sample_name      
                 ,t.catalogid as catalog_id
                 ,d.beadid as bead_id
-                ,replace(replace(convert(nvarchar(100), specabbr), '-,',''), ',-', '') as ab_sero
-                ,replace(replace(convert(nvarchar(100), specificity), '-,', ''), ',-', '') as ab_mol
-                ,rawdata as raw_value
-                ,normalvalue as baseline_value
-        from patient as p
-            join sample as s on s.PatientID = p.PatientID
-            join well as w on w.SampleID = s.SampleID
-            join tray as t on t.TrayID = w.TrayID
-            join well_detail as d on d.WellID = w.WellID
-            join product_detail as pd on pd.BeadID = d.BeadID and t.CatalogID = pd.CatalogID
+                ,replace(
+                    replace(
+                        convert(nvarchar(100), pd.specabbr), '-,',''
+                        ), ',-', ''
+                    ) as ab_sero
+                ,replace(
+                    replace(
+                        convert(nvarchar(100), pd.specificity), '-,', ''
+                        ), ',-', ''
+                    ) as ab_mol
+                ,d.rawdata as raw_value
+                ,d.normalvalue as baseline_value
+
+        from 
+
+            patient as p
+
+                join sample as s on s.PatientID = p.PatientID
+                join well as w on w.SampleID = s.SampleID
+                join tray as t on t.TrayID = w.TrayID
+                join well_detail as d on d.WellID = w.WellID
+                join product_detail as pd on pd.BeadID = d.BeadID 
+                                         and t.CatalogID = pd.CatalogID
         where 
             {kittype}
             p.patientid ='{patient_local_id}'
             {session_date}
             
-        order by shipmentdt
+        order by 
+
+            s.shipmentdt
+
         '''
-    #print(sql)
+        
     return pd.read_sql_query(sql, con=con)
-
-
-# In[11]:
 
 
 con = connect_to_fusion(fusion_connection_string(read_fusion_connection_json()))
 
 
-# In[19]:
-
-
-d = read_data_from_fusion(patient_local_id=7766
-                      ,_class=2
-                      ,date_range=['2016-01-01', '2017-04-01']
-                      ,kittype='sab')
-
-
-# In[20]:
-
-
-d.head()
-
-
-# In[22]:
-
-
-d.to_csv('1.csv')
+# d = read_data_from_fusion(patient_local_id=7766
+#                       ,_class=2
+#                       ,date_range=['2016-01-01', '2017-04-01']
+#                       ,kittype='sab')
 
